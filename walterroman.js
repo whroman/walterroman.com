ea5a0a44370b4681595235b8769f1fcab3c2118b
Walter Roman <whroman3@gmail.com>
2013-01-15 20:13:18 -0800

    Added tooltips to Musician/Devel/Human

--- walterroman.js
+++ walterroman.js
@@ -17,7 +17,7 @@ var navOnClick = function(exception) {
 	if (exception == "About") {
 		navAbout.className = "aboutClick";
 		navAbout.style.marginTop = "-60px";
-		navAbout.style.zIndex = "99"
+		navAbout.style.zIndex = "99";
 		document.getElementById("navText1").className = "navTextSelected";
 		document.getElementById("navText1").style.marginTop = "104px";
 		document.getElementById("firstLetterA").style.marginTop = "63px";
@@ -103,10 +103,12 @@ var aboutClick = function() {
 	
 	document.getElementById("aboutPage").style.visibility = "visible";
 	document.getElementById("aboutPage").style.opacity = "1";
-	document.getElementById("aboutPage").style.marginTop = "-20px";
+	document.getElementById("aboutPage").style.marginTop = "10px";
+	document.getElementById("aboutPage").style.height = "550px";
 
 	document.getElementById("contactPage").style.visibility = "hidden";
 	document.getElementById("contactPage").style.opacity = "0";
+	document.getElementById("contactPage").style.marginTop = "30px";
 
 }
 
@@ -115,11 +117,65 @@ var contactClick = function() {
 	document.getElementById("intro").style.visibility = "hidden"
 
 
-	document.getElementById("aspire").style.marginTop = "-150px";
+	document.getElementById("aspire").style.marginTop = "-140px";
 	
 	document.getElementById("contactPage").style.visibility = "visible";
 	document.getElementById("contactPage").style.opacity = "1";
+	document.getElementById("contactPage").style.marginTop = "10px";
 
 	document.getElementById("aboutPage").style.visibility = "hidden";
 	document.getElementById("aboutPage").style.opacity = "0";
+	document.getElementById("aboutPage").style.marginTop = "30px";
+	document.getElementById("aboutPage").style.height = "260px";
+
+}
+
+var toolTip = function(exception) {
+	toolTip1 = document.getElementById("toolTip1");
+	toolTip2 = document.getElementById("toolTip2");
+	toolTip3 = document.getElementById("toolTip3");
+
+	triangle1 = document.getElementById("triangle1");
+	triangle2 = document.getElementById("triangle2");
+	triangle3 = document.getElementById("triangle3");
+
+	if (exception == "toolTip1") {
+		toolTip1.className = "toolTipHover";
+		triangle1.className = "triangleHover";
+	}
+
+	if (exception == "toolTip2") {
+		toolTip2.className = "toolTipHover";
+		triangle2.className = "triangleHover";
+	}
+
+	if (exception == "toolTip3") {
+		toolTip3.className = "toolTipHover";
+		triangle3.className = "triangleHover";
+	}
+}
+
+var toolTipMouseOut = function(exception) {
+	toolTip1 = document.getElementById("toolTip1");
+	toolTip2 = document.getElementById("toolTip2");
+	toolTip3 = document.getElementById("toolTip3");
+
+	triangle1 = document.getElementById("triangle1");
+	triangle2 = document.getElementById("triangle2");
+	triangle3 = document.getElementById("triangle3");
+
+	if (exception == "toolTip1") {
+		toolTip1.className = "toolTip";
+		triangle1.className = "triangle-isosceles";
+	}
+
+	if (exception == "toolTip2") {
+		toolTip2.className = "toolTip";
+		triangle2.className = "triangle-isosceles";
+	}
+
+	if (exception == "toolTip3") {
+		toolTip3.className = "toolTip";
+		triangle3.className = "triangle-isosceles";
+	}
 }
